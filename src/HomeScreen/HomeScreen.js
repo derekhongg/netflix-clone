9d160c222ab74f3cd0209f4f1e4c1e21734d5723
derekhongg <hongderek@gmail.com>
2022-10-19 13:50:55 -0700

    first commit

---
+++ src/HomeScreen/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./HomeScreen.css";
+import Nav from '../Nav/Nav';
+
+export const HomeScreen = () => {
+    return (
+        <div className="homeScreen">
+            <Nav />
+            <h1>Test</h1>
+
+
+
+
+        </div>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDL2wQ5oFkympxQfD1_Zii39rz0dEYJDII",
+    authDomain: "netflix-clone-7a7ef.firebaseapp.com",
+    projectId: "netflix-clone-7a7ef",
+    storageBucket: "netflix-clone-7a7ef.appspot.com",
+    messagingSenderId: "654713606378",
+    appId: "1:654713606378:web:881df03ecdad056fc20793"
+};
