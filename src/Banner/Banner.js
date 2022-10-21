b5fbca37d6d39e533ecb33d3a78777d5211fa2f7
derekhongg <hongderek@gmail.com>
2022-10-21 10:28:42 -0700

    new frontend items added

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import "./Banner.css";
 import axios from 'axios';
+import instance from '../axios';
 import requests from '../Requests';
 
 const Banner = () => {
 
     const [movie, setMovie] = useState([]);
+    const url = "https://api.themoviedb.org/3"
 
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get("https://api.themoviedb.org/3" + requests.fetchNetflixOriginals);
+            const request = await axios.get(url + requests.fetchNetflixOriginals);
             setMovie(
                 request.data.results[
                     Math.floor(Math.random() * request.data.results.length - 1)
@@ -29,17 +31,17 @@ const Banner = () => {
     return (
         <header className='banner' style={{
             backgroundSize: "cover",
-            backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: "center center",
         }}>
             <div className='banner__contents'>
-                <h1 className='banner__title'>Movie Name</h1>
+                <h1 className='banner__title'>{movie?.name || movie?.original_name || movie?.title}</h1>
                 <div className='banner__buttons'>
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
                 <h1 className='banner__description'>
-                    {shortenDescription(`Test Description`, 150)}
+                    {shortenDescription(`${movie?.overview}`, 150)}
                 </h1>
             </div>
             <div className="banner__fadeBottom"/>
--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -2,16 +2,26 @@ import React from 'react';
 import "./HomeScreen.css";
 import Nav from '../Nav/Nav';
 import Banner from '../Banner/Banner';
+import requests from '../Requests';
+import Row from '../Row/Row';
 
 export const HomeScreen = () => {
+
+    const url = "https://api.themoviedb.org/3"
     return (
         <div className="homeScreen">
             <Nav />
 
             <Banner />
-            {/* Banner */}
 
-            {/* Row */}
+            <Row title="NETFLIX ORIGINALS" fetchUrl={url + requests.fetchNetflixOriginals} isLargeRow/>
+            <Row title="Trending Now" fetchUrl={url + requests.fetchTrending}/>
+            <Row title="Top Rated" fetchUrl={url + requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={url + requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" fetchUrl={url + requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchUrl={url + requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies" fetchUrl={url + requests.fetchRomanceMovies}/>
+            <Row title="Documentaries" fetchUrl={url + requests.fetchDocumentariess}/>
         </div>
     )
 }
