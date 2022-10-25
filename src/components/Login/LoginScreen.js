8e6e93d3336e0d42333a981d9dc1d5b590d8bffe
derekhongg <hongderek@gmail.com>
2022-10-24 17:25:57 -0700

    login page edits

--- src/components/Login/LoginScreen.js
+++ src/components/Login/LoginScreen.js
@@ -11,6 +11,27 @@ function LoginScreen() {
                 alt='Netflix Logo'
                 />
             </div>
+            
+            <button 
+            className='loginScreen__button'
+            >
+                Sign In
+            </button>
+            <div className='loginScreen__gradient'/>
+
+            <div className='loginScreen__body'>
+                <>
+                    <h1>Unlimited movies, TV shows, and more. </h1>
+                    <h2>Watch anywhere. At Home or On The Go.</h2>
+                    <h3>Ready to start? Enter your email below to create or restart your membership.</h3>
+                    <div className='loginScreen__input'>
+                        <form>
+                            <input type="email" placeholder='Email Address'/>
+                            <button className='loginScreen__getStarted'>GET STARTED</button>
+                        </form>
+                    </div>
+                </>
+            </div>
         </div>
     )
 }
