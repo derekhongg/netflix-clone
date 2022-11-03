ed4a4fc08a0289943250b00f4843859ee3328312
derekhongg <hongderek@gmail.com>
2022-11-03 09:59:58 -0700

    add subscription plans

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/counter/userSlice';
 import db from '../../firebase';
 
 function Plans() {
 
     const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser)
 
     useEffect(() => {
         db.collection("products")
@@ -26,11 +29,43 @@ function Plans() {
         console.log(products)
     }, []);
 
-    console.log(products, "----------");
+    const loadCheckout = async (priceId) => {
+        const docRef = await db.collection('customers')
+        .doc(user.uid)
+        .collection('checkout_sessions')
+        .add({
+            price: priceId,
+            success_url: window.location.origin,
+            cancel_url: window.location.origin,
+        })
+
+        docRef.onSnapshot(async(snap) => {
+            const {error, sessionId} = snap.data();
+
+            if(error) {
+                alert(`An error occured: ${error.message}`);
+            }
+
+            if(sessionId) {
+                const stripe = await loadStripe('')
+            }
+        })
+    }
 
     return (
         <div className='plans'>
-            {Object.entries(products).map([productId, productData])}
+            {Object.entries(products).map(([productId, productData]) => {
+                return(
+                    <div className='plans__plan'>
+                        <div className='plans__info'>
+                            <h5>{productData.name}</h5>
+                            <h6>{productData.description}</h6>
+                        </div>
+
+                        <button onClick={loadCheckout(productData.prices.priceId)}>Subscribe</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
